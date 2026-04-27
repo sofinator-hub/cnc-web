b124f83b71dd940b0e356cdf76072cd12c5f6f96
Sofia <aleeexmr11@gmail.com>
2026-04-27 10:13:47 -0600

    adding guardas page

--- app/guardas/page.tsx
+++ app/guardas/page.tsx
@@ -1,7 +1,7 @@
 export default function CNC() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Mobiliario Industrial</h1>
+      <h1 className="text-4xl font-bold">guardas industriales</h1>
     </main>
   );
 }
