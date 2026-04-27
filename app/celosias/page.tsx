f8d1f0f4fe9e1ff2c81edf3724d64d55384c3a7e
Sofia <aleeexmr11@gmail.com>
2026-04-27 11:37:06 -0600

    fixing images in gallery

--- app/celosias/page.tsx
+++ app/celosias/page.tsx
@@ -3,12 +3,12 @@ import MobileMenu from "@/components/layout/MobileMenu";
 
 export default function CelosiasPage() {
 const items = [
-{ img: "/celosias/celosia1.jpg", title: "Diseño 1" },
-{ img: "/celosias/celosia2.jpg", title: "Diseño 2" },
-{ img: "/celosias/celosia3.jpg", title: "Diseño 3" },
-{ img: "/celosias/celosia4.jpg", title: "Diseño 4" },
-{ img: "/celosias/celosia5.jpg", title: "Diseño 5" },
-{ img: "/celosias/celosia6.jpg", title: "Diseño 6" }
+{ img: "/public/celosia1.jpg", title: "Diseño 1" },
+{ img: "/public/celosia2.jpg", title: "Diseño 2" },
+{ img: "/public/celosia3.jpg", title: "Diseño 3" },
+{ img: "/public/celosia4.jpg", title: "Diseño 4" },
+{ img: "/public/celosia5.jpg", title: "Diseño 5" },
+{ img: "/public/celosia6.jpg", title: "Diseño 6" }
 ];
 
 return (
